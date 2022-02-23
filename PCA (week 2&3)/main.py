883dd42dda144f6f0a9d4102a7a1ea3dc8014292
RebeccaPel <r.n.pelsser@student.tue.nl>
2022-02-23 16:11:34 +0100

    clearer variable names

--- PCA (week 2&3)/main.py
+++ PCA (week 2&3)/main.py
@@ -13,36 +13,34 @@ from AssignmentPCA import readAllDescriptors, Plot, Covariance, CovarianceMatrix
 
 # (a) Testing Molcule
 df = pd.read_csv('QSAR_3_large_data.csv')
-mol = Molecule(df,'ppar') 
-df_target = mol.df_target
+df_ppar_full = Molecule(df,'ppar').df_target
 # (b)
-one_descriptors = mol.readMolDescriptors(2)
+random_molecule_descriptors = Molecule(df,'ppar').readMolDescriptors(2)
 
 # (c) Testing reading all descriptors
-all_descriptors = readAllDescriptors(df, 'ppar')
+df_ppar_only_descriptors = readAllDescriptors(df, 'ppar')
     
 # (d) (e) Testing Plot
 columns_of_interest = ['nHet','nS','nAB','SlogP'] #some randomly chosen descriptors
-plot = Plot(df_target,columns_of_interest)
+#plot = Plot(df_target,columns_of_interest)
 #twoDplot = plot.twoDimPlot()
 
 # (f) Testing Covariance
-lst1 = df_target['nHet'].values.tolist()
-lst2 = df_target['SlogP'].values.tolist()
-cov = Covariance(lst1,lst2)
-covariance = cov.calculateCovariance()
-covariance_test = np.cov(lst1,lst2) # If n-1 is done, the function works, but 
+lst1 = df['nHet'].values.tolist()
+lst2 = df['SlogP'].values.tolist()
+covariance_two_lists = Covariance(lst1,lst2).calculateCovariance()
+covariance_test_two_lists = np.cov(lst1,lst2) # If n-1 is done, the function works, but 
 # the covariance matrix isn't 1 on the diagonal, however when -1 is removed, the
 # covariance has the correct values...
 
 # (g) Testing Covariance matrix with scaled values
-mat = CovarianceMatrix(df,'ppar')
-mat_scaled = mat.scaleVariables()
-mat_cov = mat.covMatrix()
+covariance_matrix_class = CovarianceMatrix(df,'ppar')
+df_ppar_only_descriptors_scaled = covariance_matrix_class.scaleVariables()
+df_ppar_covariance_matrix = covariance_matrix_class.covMatrix()
 
 # (h) Testing PCA
-pca = PCA(mat_cov)
-pca_eigval, pca_eigvec = pca.perform_PCA()
+pca_class = PCA(df_ppar_covariance_matrix)
+pca_eigval, pca_eigvec = pca_class.perform_PCA()
 
 # (i) Testing plotting
 pca_plot = PCA_plot(df)
@@ -52,11 +50,6 @@ pca_plot.PCA_plot_2D()
 # (j) Testing loadings:
 load = Loadings(df,'ppar')
 X,u,s,vt = load.calculate_loadings()
-
-# I haven't been able to finish the loadings, because I can't figure out how
-# to multiply V*S, because the shapes (with ppar) are V=(32,32) and S=(26,),
-# which can never be multiplied.
-# Also because of this, I have no results to reflect on...
 
 
 
